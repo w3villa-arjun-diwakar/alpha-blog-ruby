97875aaef36a2ae4a203244045450ab0417fb3d6
Arjun Diwakar <arjun.diwakar@w3villa.com>
2023-02-01 12:53:45 +0530

    CRUD of Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,40 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
-    
+    def index
+        @articles = Article.all
+    end
+    def new
+        @article = Article.new
+    end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created succesfully!"
+            redirect_to @article
+        else
+           render 'new'
+        end 
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice]= "Article was updated succesfully!"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
 end
