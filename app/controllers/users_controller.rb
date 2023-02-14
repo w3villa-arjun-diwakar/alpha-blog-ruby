734b4aff46b8680a4e9ae23bb1869a0d798836e0
Arjun Diwakar <arjun.diwakar@w3villa.com>
2023-02-14 12:47:40 +0530

    created user index and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+    def index
+        @users =User.all
+    end
+
+    def show
+        @user =User.find(params[:id])
+        @articles = @user.articles
+    end
+
     def new
         @user = User.new
     end
@@ -15,8 +24,24 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] ="Updated successfully!"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+
+
+
     private 
     def user_params
-        params.require(:user).permit(:username, :email , :password_digest )
+        params.require(:user).permit(:username, :email , :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
-    has_many :articles
+    has_many :articles 
     validates :username, presence: true, uniqueness: { case_sensitive: false } , length: {minimum:5, maximum:25}
     validates :email, uniqueness: true, uniqueness: { case_sensitive: false } , length: {maximum:100}   
-    # has_secure_password
+    has_secure_password
+    
 end
